eeea00a79f5b0554a4b1dece2323ab4c591d383a
Michael Tobias <michaelremitobias@gmail.com>
2022-02-17 15:19:29 -0700

    added headers and response

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 const AWS = require("aws-sdk");
 exports.handler = function (event) {
   try {
-    console.log("The gateway is working");
-    console.log(23);
     console.log("event", event);
+
+    let response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT",
+      },
+      body: JSON.stringify(response),
+    };
+    console.log("response:", response);
+    return _response;
   } catch (e) {
-    throw e;
+    console.log(e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(e.message),
+    };
   }
-  return "96";
 };
