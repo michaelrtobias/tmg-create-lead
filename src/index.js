1185ccc52aa98fcfdc9fa19b993fbf63008b32d7
Michael Tobias <michaelremitobias@gmail.com>
2022-02-22 20:48:13 -0700

    quick add 5

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const createLead = require("./lambda/createLead");
 exports.handler = async (event) => {
   let { body } = event;
   console.log("typeof:", typeof body);
-  body = JSON.parse(body);
+  // body = JSON.parse(body);
   try {
     console.log("event", event);
     const results = await createLead(body);
